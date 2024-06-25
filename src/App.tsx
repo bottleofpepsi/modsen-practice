ae9297b7cd204deaa2b83b82e808f50af7d48abc
sirensong0 <smalenskimikalaj@gmail.com>
2024-06-25 21:46:03 +0300

    feat: add search results layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,27 @@
 import React from "react";
 
+import BookCard from "./components/BookCard";
 import Header from "./components/Header";
+import SearchResults from "./components/SearchResults";
 
 function App() {
     return (
         <>
             <Header />
+            <SearchResults>
+                <BookCard />
+                <BookCard />
+                <BookCard />
+                <BookCard />
+                <BookCard />
+                <BookCard />
+                <BookCard />
+                <BookCard />
+                <BookCard />
+                <BookCard />
+                <BookCard />
+                <BookCard />
+            </SearchResults>
         </>
     );
 }
--- src/components/SearchResults/index.tsx
+++ src/components/SearchResults/index.tsx
@@ -1,13 +1,28 @@
-import "./SearchResults.css";
+import "./style.css";
 
-import React, { ReactNode } from "react";
+import React, { ReactNode, useState } from "react";
 
 type Props = {
     children: ReactNode;
 };
 
 function SearchResults({ children }: Props) {
-    return <main className="results">{children}</main>;
+    return (
+        <>
+            <ResultCount />
+            <main className="results">{children}</main>
+        </>
+    );
+}
+
+function ResultCount() {
+    const [count] = useState(0);
+
+    return (
+        <section className="result-count">
+            <span className="result-count-text">Found {count} books</span>
+        </section>
+    );
 }
 
 export default SearchResults;
