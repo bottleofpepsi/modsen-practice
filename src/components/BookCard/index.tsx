e3c41f96b91bad5f141707e201b2cfd1a41874da
sirensong0 <smalenskimikalaj@gmail.com>
2024-06-25 21:44:59 +0300

    feat: add book card layout

--- src/components/BookCard/index.tsx
+++ src/components/BookCard/index.tsx
@@ -1,4 +1,4 @@
-import "./BookCard.css";
+import "./style.css";
 
 import React from "react";
 
@@ -7,9 +7,12 @@ import placeholder from "../../assets/placeholder.jpg";
 function BookCard() {
     return (
         <article className="book-card">
-            <img src={placeholder} alt="Book Image" width="200" height="200" />
-            <h3>Template</h3>
-            <p>Author</p>
+            <img src={placeholder} alt="Book Image" width="250" height="250" />
+            <div className="book-info">
+                <h3 className="book-name">Book Name</h3>
+                <p className="book-category">Computers</p>
+                <p className="book-authors">Author</p>
+            </div>
         </article>
     );
 }
