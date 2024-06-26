90b25877cf0f011d7aa92a5a5dd7cd4dc51521a9
sirensong0 <smalenskimikalaj@gmail.com>
2024-06-26 21:59:15 +0300

    fix: FINALLY a working version of dotenv configuration

--- webpack.config.ts
+++ webpack.config.ts
@@ -3,43 +3,43 @@ import Dotenv from "dotenv-webpack";
 import path from "path";
 import { Configuration } from "webpack";
 
-module.exports = () => {
-  const config: Configuration = {
-    entry: "./src/main.tsx",
-    output: {
-      filename: "bundle.js",
-      path: path.resolve(__dirname, "dist"),
-    },
-    resolve: {
-      extensions: [".tsx", ".ts", ".js"],
-    },
-    module: {
-      rules: [
-        {
-          test: /\.tsx?$/,
-          use: "ts-loader",
-          exclude: /node_modules/,
-        },
-        {
-          test: /\.css$/,
-          use: ["style-loader", "css-loader"],
-        },
-        { 
-          test: /\.(png|jp(e*)g|svg|gif)$/, 
-          type: "asset/resource",
-        }
-      ],
-    },
-    mode:
-      (process.env.NODE_ENV as "production" | "development" | undefined) ??
-      "development",
-    plugins: [
-      new CopyWebpackPlugin({
-        patterns: [{ from: "public" }],
-      }),
-      new Dotenv()
+const config: Configuration = {
+  entry: "./src/main.tsx",
+  output: {
+    filename: "bundle.js",
+    path: path.resolve(__dirname, "dist"),
+  },
+  resolve: {
+    extensions: [".tsx", ".ts", ".js"],
+  },
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/,
+        use: "ts-loader",
+        exclude: /node_modules/,
+      },
+      {
+        test: /\.css$/,
+        use: ["style-loader", "css-loader"],
+      },
+      { 
+        test: /\.(png|jp(e*)g|svg|gif)$/, 
+        type: "asset/resource",
+      }
     ],
-  };
+  },
+  mode:
+    (process.env.NODE_ENV as "production" | "development" | undefined) ??
+    "development",
+  plugins: [
+    new CopyWebpackPlugin({
+      patterns: [{ from: "public" }],
+    }),
+    new Dotenv({
+      systemvars: true
+    })
+  ],
+};
 
-  return config;
-}
+export default config;
