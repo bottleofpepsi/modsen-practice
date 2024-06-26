370ced9ee83710620671fe659735b83898a40b5e
sirensong0 <smalenskimikalaj@gmail.com>
2024-06-26 19:16:23 +0300

    chore: update webpack configuration

--- webpack.config.ts
+++ webpack.config.ts
@@ -1,59 +1,64 @@
 import CopyWebpackPlugin from "copy-webpack-plugin";
-import dotenv from "dotenv";
-import * as fs from "fs";
+import Dotenv from "dotenv-webpack";
+// import * as fs from "fs";
 import path from "path";
-import { Configuration, DefinePlugin } from "webpack";
+// import { argv, env } from "process";
+import { Configuration } from "webpack";
 
-type envObject = {
-  [key: string]: string,
-}
+// type envObject = {
+//   [key: string]: string,
+// }
 
-const currentPath = path.join(__dirname);
-const basePath = currentPath + "/.env";
-const envPath = basePath + "." + process.env.ENVIRONMENT;
-const finalPath = fs.existsSync(envPath) ? envPath : basePath;
+module.exports = () => {
+  // const currentPath = path.join(__dirname);
+  // const basePath = currentPath + "/.env";
+  // const envPath = basePath + "." + process.env.ENVIRONMENT;
+  // const finalPath = fs.existsSync(envPath) ? envPath : basePath;
+  
+  // const fileEnv = dotenv.config({ path: finalPath }).parsed;
+  // const envKeys = Object.keys(fileEnv as envObject).reduce((prev: envObject, next: string) => {
+  //   prev[`process.env.${next}`] = JSON.stringify((fileEnv as envObject)[next]);
+  //   return prev;
+  // }, {});
 
-const fileEnv = dotenv.config({ path: finalPath }).parsed;
-const envKeys = Object.keys(fileEnv as envObject).reduce((prev: envObject, next: string) => {
-  prev[`process.env.${next}`] = JSON.stringify((fileEnv as envObject)[next]);
-  return prev;
-}, {});
-
-const config: Configuration = {
-  entry: "./src/main.tsx",
-  output: {
-    filename: "bundle.js",
-    path: path.resolve(__dirname, "dist"),
-  },
-  resolve: {
-    extensions: [".tsx", ".ts", ".js"],
-  },
-  module: {
-    rules: [
-      {
-        test: /\.tsx?$/,
-        use: "ts-loader",
-        exclude: /node_modules/,
-      },
-      {
-        test: /\.css$/,
-        use: ["style-loader", "css-loader"],
-      },
-      { 
-        test: /\.(png|jp(e*)g|svg|gif)$/, 
-        type: "asset/resource",
-      }
+  const config: Configuration = {
+    entry: "./src/main.tsx",
+    output: {
+      filename: "bundle.js",
+      path: path.resolve(__dirname, "dist"),
+    },
+    resolve: {
+      extensions: [".tsx", ".ts", ".js"],
+    },
+    module: {
+      rules: [
+        {
+          test: /\.tsx?$/,
+          use: "ts-loader",
+          exclude: /node_modules/,
+        },
+        {
+          test: /\.css$/,
+          use: ["style-loader", "css-loader"],
+        },
+        { 
+          test: /\.(png|jp(e*)g|svg|gif)$/, 
+          type: "asset/resource",
+        }
+      ],
+    },
+    mode:
+      (process.env.NODE_ENV as "production" | "development" | undefined) ??
+      "development",
+    plugins: [
+      new CopyWebpackPlugin({
+        patterns: [{ from: "public" }],
+      }),
+      new Dotenv({
+        path: "dist/.env"
+      })
     ],
-  },
-  mode:
-    (process.env.NODE_ENV as "production" | "development" | undefined) ??
-    "development",
-  plugins: [
-    new CopyWebpackPlugin({
-      patterns: [{ from: "public" }],
-    }),
-    new DefinePlugin(envKeys)
-  ],
-};
+  };
 
-export default config;
+  return config;
+}
